f6260b58b3cd61aa5c439c9ed3b75e5c06b6dc48
Javier Calderon <jcalderon@360digital.co>
2016-02-29 17:31:38 -0500

    Creating a unit test class

---
+++ WowUtilsTests.m
@@ -0,0 +1,71 @@
+//
+//  WowUtilsTests.m
+//  GuildBrowser
+//
+//  Created by Javier Calderon on 29/02/16.
+//  Copyright © 2016 Charlie Fulton. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "WowUtils.h"
+
+@interface WowUtilsTests : XCTestCase
+
+@end
+
+@implementation WowUtilsTests
+
+
+// 1
+-(void)testCharacterClassNameLookup
+{
+    // 2
+    XCTAssertEqualObjects(@"Warrior",
+                         [WoWUtils classFromCharacterType:1],
+                         @"ClassType should be Warrior");
+    // 3
+    XCTAssertFalse([@"Mage" isEqualToString:[WoWUtils classFromCharacterType:2]]);
+    
+    // 4
+    XCTAssertTrue([@"Paladin" isEqualToString:[WoWUtils classFromCharacterType:2]]);
+    // add the rest as an exercise
+}
+
+- (void)testRaceTypeLookup
+{
+    XCTAssertEqualObjects(@"Human", [WoWUtils raceFromRaceType:1]);
+    XCTAssertEqualObjects(@"Orc", [WoWUtils raceFromRaceType:2]);
+    XCTAssertFalse([@"Night Elf" isEqualToString:[WoWUtils raceFromRaceType:45]]);
+    // add the rest as an exercise
+}
+
+- (void)testQualityLookup
+{
+    XCTAssertEqual(@"Grey", [WoWUtils qualityFromQualityType:1]);
+    XCTAssertFalse([@"Purple" isEqualToString:[WoWUtils qualityFromQualityType:10]]);
+    // add the rest as an exercise
+}
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
